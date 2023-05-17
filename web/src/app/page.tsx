f35706780e187c2f92367bab74241bcf0124abfb
Jean Carlos de Oliveira <jeancarlosdeoliveira@outlook.com>
2023-05-17 03:30:43 -0300

    Aula 01 - Web

---
+++ web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-zinc50 h-screen bg-zinc-950">
+      <h1 className="text-4xl font-bold">Sua capsula do tmepo</h1>
+    </div>
+  )
+}
---
+++ web/src/components/Button.tsx
@@ -0,0 +1,9 @@
+const styles = {
+    color: '#F00'
+}
+
+export function Button() {
+    return (
+        <p style={styles}>Button</p>
+    )
+}
